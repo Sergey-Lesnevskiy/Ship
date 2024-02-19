5d0e8621e74bd10065e1ed0d53f930b3d4b7ebdb
Lesnevsky-Sergey <78030028+Sergey-Lesnevskiy@users.noreply.github.com>
2024-02-19 20:01:52 +0300

    feat: add project with ws

--- index.ts
+++ index.ts
@@ -5,24 +5,24 @@ import WebSocket, { WebSocketServer } from 'ws';
 const HTTP_PORT = 8181;
 const WS_PORT = 3000;
 
-console.log(`Start static http server on the ${HTTP_PORT} port!`);
+console.log(`Start server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
 
-const wsServer: WebSocket.Server<typeof WebSocket, typeof IncomingMessage> = new WebSocketServer({ port: WS_PORT });
+const WSServer: WebSocket.Server<typeof WebSocket, typeof IncomingMessage> = new WebSocketServer({ port: WS_PORT });
 
-wsServer.on('connection', onConnect);
-wsServer.on('close', onClose);
+WSServer.on('connection', onConnect);
+WSServer.on('close', onClose);
 
 function onConnect(ws: WebSocket.Server<typeof WebSocket, typeof IncomingMessage>): void {
   ws.on('error', console.error);
 
-  console.log(`WebSocket server works on ${WS_PORT}`);
+  console.log(`WS works on ${WS_PORT}`);
 
   ws.on('message', function message(data) {
-    console.log('Data received: ', JSON.parse(data.toString()));
+    console.log('Data: ', JSON.parse(data));
   });
 }
 
 function onClose(): void {
-  console.log('WebSocket closed');
+  console.log('WS closed');
 }
